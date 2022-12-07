5622f3619fb8cba8188f59bb7bddc9b0942f450b
RealXun <112812176+RealXun@users.noreply.github.com>
2022-12-07 02:24:56 +0100

    Update

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -5,7 +5,9 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-
+'''
+To predict the missing information in the rank column
+'''
 def predict(df):
 
     # df with the zero values in rank column
@@ -19,7 +21,7 @@ def predict(df):
     x = clean_df[['Score','Scored_by']].values
     y = clean_df['Rank'].values
 
-    # fit my model
+    # fit my model (adjusting the parameters in the model to improve accuracy.)
     lm = LinearRegression()
     lm.fit(x, y)
 
@@ -36,6 +38,10 @@ def predict(df):
     df.loc[missing_index, 'Rank'] = lm.predict(predict_x)
 
 
+
+'''
+Convert the string time to just minutes
+'''
 def to_minutes(df):
     df['Duration'] = df['Duration'].apply(lambda positions : positions.split(' ')).apply(lambda positions : positions[0] if len(positions) <= 1 else # is lenght of the word 1 or is less than 1 then zero
             (int(positions[0]) / 60 if positions[1] == 'sec' else # If position 1 is sec, then we devide position 1 by 60
@@ -45,6 +51,10 @@ def to_minutes(df):
             int(positions[0]) * 60  )))))
 
 
+
+'''
+Pichart unidemensioanl
+'''
 def pieplot(data, graph_name):
     # Create an axis object
     fig, ax = plt.subplots(1, 1, figsize = (15, 8))
@@ -52,8 +62,8 @@ def pieplot(data, graph_name):
     # Get the data in the right format
     piedata = Counter(data)
 
-    height = list(piedata.keys())
-    bars  = list(piedata.values())
+    height = list(piedata.keys()) # Assign keys to height
+    bars  = list(piedata.values())# Assign values to bars
     
     # Assign explode value automatically depending on the data and
     # declaring the exploding pie to separate one of the parts, in this case, the higher
@@ -62,43 +72,62 @@ def pieplot(data, graph_name):
     length =len(bars)
     explode_value = tuple([0 if i!=max_index else 0.2 for i in range(length)])
 
-    # Plot the pie chart
-    ax.pie(bars, labels = height, shadow = False, explode=explode_value ,startangle = 0, autopct = "%1.2f%%")
+    ax.pie(bars, labels = height, shadow = False, explode=explode_value ,startangle = 0, autopct = "%1.2f%%") # Plot the pie chart
 
-    #ax.axis('equal')
     ax.set_title(f"Pie chart for {graph_name} category.")
-    
-    # Save the plot to an image
 
-    # Show graphic
-    plt.show()
+    plt.show() # Show graphic
 
-    # Close the plot
-    plt.close()
+    plt.close()# Close the plot
 
+
+
+'''
+Barplot unidemensioanl
+'''
 def simple_barplot(data, graph_name):
-    # Process the genre column to split on comma and append resulting
-    # genres all to a single list
-    # all_genres = []
-    # for item in anime_df["Genre"]:
-    #     item = item.strip()
-    #     all_genres.extend(item.split(','))
 
-
-    # creating a dict from the data
     bar_date_dict = Counter(data)
 
     barplot_df=pd.DataFrame.from_dict(bar_date_dict,orient='index').reset_index() # creating a df from
 
     barplot_df=barplot_df.rename(columns={'index':graph_name, 0:'Count'}) # renaming the columns of the df
 
-    bar_plot = sns.barplot(x=barplot_df[graph_name],y=barplot_df["Count"]) #assigning values to the plot
+    bar_plot = sns.barplot(x=barplot_df["Count"],y=barplot_df[graph_name]) #assigning values to the plot
 
     bar_plot.bar_label(bar_plot.containers[0],label_type='edge', fontsize=6) # Show the values of each bar at their top
 
     plt.xticks(rotation=90) # rotate the name of the bars
     plt.show()
 
+
+
+'''
+Countplot unidmensional where the information taken is a list
+'''
+def countplot(df,column):
+    # Process the genre column to split on comma and append resulting
+    # genres all to a single list
+    all = []
+    for item in df:
+        item = item.strip()
+        all.extend(item.split(','))
+
+    df_all = pd.DataFrame (all, columns = [column]) # Creation of a df in order to be able to order by descending in the plot
+
+    # Plot the genres and their count respectively
+    fig, ax = plt.subplots(1, 1, figsize = (20, 10))
+    sns.countplot(y= column, data = df_all, palette=['#FF9999',"#99CC99"],order=df_all[column].value_counts().index)
+
+    # Set the axes and title of the plot
+    plt.xticks(rotation = 45)
+    plt.tight_layout()
+
+
+
+'''
+Barplot unidmensional where the information taken is a list
+'''
 def complex_barplot(data, graph_name):
     # Process the genre column to split on comma and append resulting
     # genres all to a single list
@@ -121,27 +150,30 @@ def complex_barplot(data, graph_name):
     plt.xticks(rotation=90) # rotate the name of the bars
     plt.show()
 
+
+
+'''
+Barplot for top 10 Unidimensional
+'''
 def complex_barplot_top10(data):
-    # Process the genre column to split on comma and append resulting
-    # genres all to a single list
     splitted_data = data.str.split(',').explode().value_counts().head(10)
     splitted_index = data.str.split(',').explode().value_counts().head(10).index
 
-    # creating a dict from the data
-
-    plt.figure(figsize=(10,8))
+    plt.figure(figsize=(20, 6))
 
     bar_plot = sns.barplot(y = splitted_index , x = splitted_data )
 
     bar_plot.bar_label(bar_plot.containers[0],label_type='edge', fontsize=6) # Show the values of each bar at their top
 
+
+
+'''
+Barplot for top 10 Unidimensional
+'''
 def barplot_top10(data):
-    # Process the genre column to split on comma and append resulting
-    # genres all to a single list
+
     splitted_data = data.value_counts().head(10)
     splitted_index = data.value_counts().head(10).index
-
-    # creating a dict from the data
 
     plt.figure(figsize=(10,8))
 
@@ -149,34 +181,80 @@ def barplot_top10(data):
 
     bar_plot.bar_label(bar_plot.containers[0],label_type='edge', fontsize=6) # Show the values of each bar at their top
 
+
+
+'''
+Boxplot unidimensinal
+'''
 def box(df,cat):
     plt.figure(figsize=(6,6))
     sns.boxplot(data = df , y = cat)
 
 
 
+'''
+Find the top ten of a categorical column taking into reference a numerical column
+'''
+def top_10(df,x,y): # we pass de dataframe, the categorical column and the numerical column
+    return df.groupby([x] , as_index=False).agg({y : 'median'}).sort_values(y , ascending = False).head(10)
 
 
-#### OLD ONES
+'''
+Find the top ten of a categorical column taking into reference a numerical column
+'''
+def top_10_multi(df,x,y,z): # we pass de dataframe, the categorical column and the numerical column
+    return df.groupby([x] , as_index=False).agg({y : 'median', z: 'median'}).sort_values(y , ascending = False).head(10)
 
 
-def split(df,categ):
-    categ_list = []
-    for item in df[categ]:
-        item = item.strip().split(',')
-        categ_list.extend(item)
-    list_data = Counter(categ_list)
-    return list_data
+'''
+Boxplot bidimensional
+'''
+def box_bidi(df,value1,value2):
+    plt.figure(figsize=(20,8))
+    return sns.boxplot(x= value1 , y = value2, data = df , order= top_10(df,value1,value2)[value1] )
 
-def barplot(df, xcolname, ycolname, cmap = 'YlGnBu', quantity = "occurrences"):
 
-    y_pos = np.arange(len(ycolname))
 
-    # Create bars
-    plt.bar(y_pos, ycolname)
+'''
+Scaterplot
+'''
+def scat(df_copy,value1,value2):
+    plt.figure(figsize=(15,5))
+    return sns.scatterplot(data = df_copy , x = value1 , y= value2 , hue = value2 , size = value2 , sizes = (20,200))
 
-    # Create names on the x-axis
-    plt.xticks(y_pos, xcolname)
 
-    # Show graphic
-    plt.show()
+
+
+'''
+Funciton to extract the data of a columns with respect to another column.
+
+'''
+def series_extract(df,col_index_name , col_target_name): #Col name is the column to which we want to extract the data.
+
+  datos = [] #Store the data
+  cont = 0
+  index_column = df.columns.get_loc(col_target_name) #To know the index of the desired column
+
+  for list_gen in df[col_index_name]:
+
+    for gen in list_gen:
+        
+        val = df.iloc[cont , index_column ] #Find the value of the desired column 
+        datos.append(val) #Store values from the value to the data list
+
+    cont += 1   
+
+  return pd.Series(datos)
+
+
+'''
+Boxplot
+'''
+def aux(df,x,y):
+    get_score = series_extract(df,'Studios_Split', 'Score')
+    df_anime_aux3 = pd.DataFrame() #New dataframe
+    #Set the producers columns with their score
+    df_anime_aux3[x] = df[x].str.split(',').explode()
+    df_anime_aux3[y] = get_score
+
+    return df_anime_aux3.head()
